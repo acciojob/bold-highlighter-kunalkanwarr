08944f23857bd59deb9ec9a319e1351f7d34ebc3
Kunal Kanwar <113274734+kunalkanwarr@users.noreply.github.com>
2024-12-19 16:17:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function highlight() {
     //Write your code here
-
+// Get all <strong> elements (bold words)
+    const boldWords = document.querySelectorAll('strong');
+    
+    // Loop through each <strong> element and change its color to green
+    boldWords.forEach((word) => {
+        word.style.color = 'green';
+    });
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+// Get all <strong> elements (bold words)
+    const boldWords = document.querySelectorAll('strong');
+    
+    // Loop through each <strong> element and revert its color to black
+    boldWords.forEach((word) => {
+        word.style.color = 'black';
+    });
     
 }
